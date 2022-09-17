8388e72ba2a09ab7f82ff72460a607738b6cb1fe
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-09-17 12:35:26 +1000

    Minor changes to authenticity

--- blandbook-server/app/controllers/application_controller.rb
+++ blandbook-server/app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
     
     #these are part of the knock-based authentication
-    before_action :authenticate_user
+    skip_before_action :verify_authenticity_token
+
+    include Knock::Authenticable
 
     def current
         render json: current_user
