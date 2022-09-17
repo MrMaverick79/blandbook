a46bb4aa51ea2582e1fe5573f32607357c9d6d49
hwlei888 <leihuawen888@gmail.com>
2022-09-17 12:21:05 +1000

    database

--- blandbook-server/config/routes.rb
+++ blandbook-server/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
   root to: 'pages#home'
 
@@ -7,8 +6,6 @@ Rails.application.routes.draw do
 
   get '/alldata' => 'users#allData'
 
-=======
   post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 301f61ba21b547269f301d0a681b3c646208f5bb
 end
