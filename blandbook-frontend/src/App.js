b9cef051362fef4b7516e65c8d92ecbf39899dd3
hwlei888 <leihuawen888@gmail.com>
2022-09-18 22:02:33 +1000

    test

--- blandbook-frontend/src/App.js
+++ blandbook-frontend/src/App.js
@@ -1,61 +1,120 @@
-// import logo from './logo.svg';
+
 import './App.css';
-import { Route, HashRouter as Router, Link } from 'react-router-dom';
-
-import LoginMain from './components/LoginMain';
-
-
-function App() {
+import { Route, HashRouter as Router, Link, Redirect } from 'react-router-dom';
+import ChatroomShow from './components/ChatroomShow';
+import React from 'react';
+import axios from 'axios';
 
 
-  const homeIcon = <span class="material-symbols-outlined">home_app_logo</span>
-  const settingIcon = <span class="material-symbols-outlined">settings</span>
-  const accountIcon = <span class="material-symbols-outlined">account_circle</span>
-  const chatIcon = <span class="material-symbols-outlined">chat</span>
-  const groupChatIcon = <span class="material-symbols-outlined">forum</span>
-  const weatherIcon = <span class="material-symbols-outlined">nest_farsight_weather</span>
-  const calendarIcon = <span class="material-symbols-outlined">calendar_month</span>
+const homeIcon = <span class="material-symbols-outlined">home_app_logo</span>
+const settingIcon = <span class="material-symbols-outlined">settings</span>
+const accountIcon = <span class="material-symbols-outlined">account_circle</span>
+const chatIcon = <span class="material-symbols-outlined">chat</span>
+const groupChatIcon = <span class="material-symbols-outlined">forum</span>
+const weatherIcon = <span class="material-symbols-outlined">nest_farsight_weather</span>
+const calendarIcon = <span class="material-symbols-outlined">calendar_month</span>
 
-  return (
-    <div className="App">
-      {/* This is the template layout */}
+class App extends React.Component {
 
-      <Router>
-        <div className='container'>
-          {/* flex container */}
-          <nav>
-            {/* all a_tags here */}
-            <Link to="#">{settingIcon}</Link>
-            <Link to="#">{homeIcon}</Link>  
-            <Link to="#">{accountIcon}</Link>
-            <Link to="#">{chatIcon}</Link>
-            <Link to="#">{groupChatIcon}</Link>
-            <Link to="#">{weatherIcon}</Link>
-            <Link to="#">{calendarIcon}</Link>
-          </nav>
-
-          <main>
-            {/* all Components here */}
-
-            <LoginMain />
-            <div className='test'>DIV test</div>
-          </main>
-
-        </div>
-        {/* flex container end */}
+    state={
+      currentUser: true, //return to null after testing
+      allRooms: [],
+      currentRoom: {
+        chatroom: {},
+        users: [], 
+        messages: []
+      }
+    }
 
 
-        <footer className='center'>
-          {/* footer info here */}
-          &copy; Blandbook @ 2022
-        </footer>
+    //I could probably use axios here and we already have the json. This is for the chatroom. The id is fetched from the url, so url must match the room_id 
+    getRoomData = async( id ) => {
 
 
-       
+       const res  = await axios.get(`http://localhost:3000/chatrooms/${id}.json`)
+       console.log(res);
+       this.setState({
+              currentRoom:{
+                  chatroom: res.data,
+                  users: res.data.users,
+                  messages: res.data.messages
 
-      </Router>
-    </div>
-  );
-}
+                } //end currentRoom 
+
+              }); //end setState
+
+        
+  } //end getRoom Data
+
+
+  updateAppStateRoom = (newRoom) => {
+    this.setState ({
+      currentRoom:{
+        chatroom: newRoom.chatroom.data,
+        users:newRoom.users,
+        messages: newRoom.messages
+      }
+    })
+  }
+
+
+  render(){
+
+
+    return (
+          <div className="App">
+            {/* This is the template layout */}
+
+            <Router>
+              <div className='container'>
+                {/* flex container */}
+                <nav>
+                  {/* all a_tags here */}
+                  <Link to="#">{settingIcon}</Link>
+                  <Link to="#">{homeIcon}</Link>  
+                  <Link to="#">{accountIcon}</Link>
+                  <Link to="/chatrooms/:id">{chatIcon}</Link> 
+                  <Link to="#">{groupChatIcon}</Link>
+                  <Link to="#">{weatherIcon}</Link>
+                  <Link to="#">{calendarIcon}</Link>
+                </nav>
+
+                <main>
+                  {/* all Components here */}
+                  <div className='test'>DIV test</div>
+                    <Route exact path ="/chatrooms/:id" render={ (props) => { return this.state.currentUser ?
+                    (<ChatroomShow
+                        {...props}
+                        cableApp ={this.props.cableApp}
+                        updateApp ={this.updateAppStateRoom}
+                        getRoomData ={this.getRoomData}
+                        roomData = {this.state.currentRoom}
+                        currentUser = {this.state.currentUser}
+                      />
+                      
+                      ) : (
+                        <Redirect to ='/' />
+                      )
+                      }} />
+                    
+                </main>
+
+              </div>
+              {/* flex container end */}
+
+
+              <footer className='center'>
+                {/* footer info here */}
+                &copy; Blandbook @ 2022
+              </footer>
+
+
+
+
+            </Router>
+          </div>
+        );
+    }
+} 
 
 export default App;
--- blandbook-frontend/src/index.js
+++ blandbook-frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import * as serviceWorker from './serviceWorkerRegistration';
+import actionCable from 'actioncable';
+
+const CableApp = {}
+
+CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App cableApp={CableApp}/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister()
